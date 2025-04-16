5655f2e546ee09db7217a2d0c705b6f29043f010
rbitar1 <reenabitar1@gmail.com>
2025-04-16 21:44:42 +0000

    started nodes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
 
-
 import './App.css'
+import Tree from './Homework/hmwk.tsx'; 
 
 function App() {
+  return (
+    <div className="App">
+      <h1>Homework Tree</h1>
+      <Tree />
+    </div>
+  );
 }
 
 export default App
