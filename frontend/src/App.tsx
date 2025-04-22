0e582d12f7f1e5addafe2b080b301047eb6a3761
Amogh Samaga <57379492+asamaga18@users.noreply.github.com>
2025-04-22 22:59:06 +0000

    changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,6 +17,7 @@ function App() {
     <Router>
       <Routes>
         <Route path = "" element={<SignIn />} />
+        <Route path = "/Landing" element={<Landing />} />
       </Routes>
     </Router>
   );
--- frontend/src/SignIn/SignIn.tsx
+++ frontend/src/SignIn/SignIn.tsx
@@ -1,9 +1,15 @@
 import { useState } from 'react';
 import "./SignIn.css"
 
+const handleLogin = () => {
+    window.location.href = 'https://redesigned-bassoon-j77q7ww9v5j2j76x-5173.app.github.dev/login';
+};
+
 const SignIn = ()=> {
     return (
-
+        <div>
+            <button onClick={handleLogin}>Login with Google</button>
+        </div>
     );
 }
 
