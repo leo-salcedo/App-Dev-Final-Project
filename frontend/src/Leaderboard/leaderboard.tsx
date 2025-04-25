00fafcf5f296986f1a962cc0d2484e5c2c7a6c5d
Mags-09 <mcandmag@terpmail.umd.edu>
2025-04-25 21:47:34 +0000

    made sidebar, edited color scheme on profile

--- frontend/src/Leaderboard/leaderboard.tsx
+++ frontend/src/Leaderboard/leaderboard.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+import Sidebar from '../Sidebar/Sidebar.tsx';
 import './leaderboard.css'
 
 const Leaderboard = ()=> {
     return (
         <div className="leaderboard">
+            <Sidebar />
             <h2>Leaderboard</h2>
         </div>
     );
