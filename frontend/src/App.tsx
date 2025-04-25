7712f4d33cdf8a17c79a93c64a943d0c5ded3f05
Amogh Samaga <57379492+asamaga18@users.noreply.github.com>
2025-04-25 19:28:04 +0000

    updated env pulling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css'
 import SignIn from "./SignIn/SignIn.tsx"
 import Tree from './Homework/hmwk.tsx'; 
-const backendUrl = process.env.REACT_APP_BACKEND;
+const backendUrl = import.meta.env.VITE_BACKEND;
 import Leaderboard from "./Leaderboard/leaderboard.tsx"
 import Questions from "./Questions/questions.tsx"
 
@@ -18,17 +18,14 @@ function App() {
   return (
     <Router>
       <Routes>
-<<<<<<< HEAD
         <Route path = "" element={<SignIn />} />
         <Route path = "/Landing" element={<Landing />} />
         <Route path = "/Homework" element = {<Tree/>}/>
         <Route path = "/Leaderboard" element = {<Leaderboard/>}></Route>
-        <Route path = "/Questions" element = {<Questions/>} ></Route>
+        {/* <Route path = "/Questions" element = {<Questions/>} ></Route> */}
 
-=======
         <Route path = "/" element={<SignIn />} />
         <Route path = "/tree" element={<Tree />} />
->>>>>>> cec24bb828d0433d597acf01b7ab93f9879d3ffa
       </Routes>
     </Router>
   );
