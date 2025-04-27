475ed9934aee3f7bebb03ee3e343c647626e01db
rbitar1 <reenabitar1@gmail.com>
2025-04-27 22:43:55 +0000

    updated homework

--- frontend/src/Homework/hmwk.tsx
+++ frontend/src/Homework/hmwk.tsx
@@ -9,89 +9,95 @@ type TreeNode = {
 };
 
 const treeData: TreeNode = {
-  label: 'Start',
+  label: 'Bootcamp Homework',
   color: 'not-started',
   children: [
       {
-          label: 'Homework 1A',
+          label: '1A',
           color: 'completed',
           children: [
-              { label: 'Homework 1B', color: 'not-started' },
-              { label: 'Homework 1C', color: 'not-started' },
+              { label: '1B', color: 'not-started' },
+              { label: '1C', color: 'not-started' },
           ],
       },
       {
-          label: 'Homework 2A',
+          label: '2A',
           color: 'completed',
           children: [
-              { label: 'Homework 2B', color: 'not-started' },
+              { label: '2B', color: 'not-started' },
           ],
       },
       {
-        label: 'Homework 3A',
+        label: '3A',
         color: 'completed',
         children: [
-            { label: 'Homework 3B', color: 'not-started' },
+            { label: '3B', color: 'not-started' },
         ],
     }, 
     {
-      label: 'Homework 4A',
+      label: '4A',
       color: 'completed',
       children: [
-          { label: 'Homework 4B', color: 'not-started' },
+          { label: '4B', color: 'not-started' },
       ],
     },
     {
-      label: 'Homework 5A',
+      label: '5A',
       color: 'completed',
     },
     {
-      label: 'Homework 6A',
+      label: '6A',
       color: 'completed',
     },
     {
-      label: 'Homework 7A',
+      label: '7A',
       color: 'completed',
       children: [
-        { label: 'Homework 7B', color: 'not-started' },
-        { label: 'Homework 7C', color: 'not-started' },
+        { label: '7B', color: 'not-started' },
+        { label: '7C', color: 'not-started' },
       ],
-  },
+    },
   ],   
 };
 
 // you should make the nodes change dynamically and scale with the screen
+const click = (label: string) => {
+  if (label === "Bootcamp Homework"){
+    return;
+  }
+  window.location.href = '/homework/${label}';
+};
+
 function Tree(){
   const makeTree = (node: TreeNode) => {
     return (
       <div className = "tree-node">
-        <div className="node-content">
-        <div className={`triangle ${node.color}`} onClick={() => alert(node.label)}></div>
+        <div className={node.label === "Bootcamp Homework" ? "rectangle" : "triangle"} onClick={() => click(node.label)}>
+          <div className={node.label === "Bootcamp Homework" ? "rectangle-text" : "triangle-text"}>
+            {node.label}
+          </div>
         </div>
-        
+
+
         {node.children && (
           <div className = "tree-children">
             {node.children.map((child, i) => (
-              <div key = {i} className =  "tree-branch">
-                <div className = "connector-line" />
-                {makeTree(child)}
+              <div key = {i} className="tree.branch">
+                <div className = {
+                  node.children.length === 2 ? 
+                  i === 0 ? 'line-left' : 'line-right' 
+                  : 'line-straight'
+                }></div>
+              {makeTree(child)}
               </div>
             ))}
           </div>
         )}
       </div>
-    );
+   );
   };
-  return (
-    <div className = "page-container">
-      <Sidebar />
-      <div className = "tree-container">
-        {makeTree(treeData)}
-      </div>
-    </div>
-  );
+  return <div className = "tree-container"> {makeTree(treeData)}</div>;
 }
-
 export default Tree;
 
 
